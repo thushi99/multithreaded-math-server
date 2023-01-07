a8685844475c15a0e2d61a6f0569c3504d1adbdc
thushitharan <thushitharansutha@gmail.com>
2023-01-07 12:20:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Run echo_server file
+    python3 mathserver.py
+    
+## Run telnet file
+    telnet localhost 8877
+
+## Try Math expressions
+    Example:
+        1+2
+        a=2
+        b=5
+        a+b
---
+++ mathserver.py
@@ -0,0 +1,69 @@
+import socket
+from subprocess import Popen, STDOUT, PIPE
+from threading import Thread
+
+def start_new_math_thread(conn, addr):
+    t = MathServerCommunicationThread(conn, addr)
+    t.start()
+
+class ProcessOutputThread(Thread):
+    def __init__(self, proc, conn):
+        Thread.__init__(self)
+        self.proc = proc
+        self.conn = conn
+
+    def run(self):
+        while not self.proc.stdout.closed and not self.conn._closed:
+            try:
+                self.conn.sendall(self.proc.stdout.readline())
+            except:
+                pass
+
+class MathServerCommunicationThread(Thread):
+    def __init__(self, conn, adr):
+        Thread.__init__(self)
+        self.conn = conn
+        self.addr = addr
+
+    def run(self):
+        print("{} Connected with back port {}".format(self.addr[0], self.addr[1]))
+        self.conn.sendall("Multi Threaded Math Server developed by Thushitharan\n\nGive any Math expressions, and I will answer you :)\n\n$".encode())
+
+        p = Popen(['bc'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        output = ProcessOutputThread(p, self.conn)
+        output.start()
+
+        while not p.stdout.closed or not self.conn._closed:
+            try:
+                data = self.conn.recv(1024)
+                if not data:
+                    break
+                else:
+                    try:
+                        data = data.decode()
+                        query = data.strip()
+                        if query == 'quit' or query == 'exit':
+                            p.communicate(query.encode(), timeout=1)
+                            if p.poll() is not None:
+                                break
+                        query = query + '\n'
+                        p.stdin.write(query.encode())
+                        p.stdin.flush()
+                    except:
+                        pass
+            except:
+                pass
+        self.conn.close()
+
+HOST = ''
+PORT = 8877
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen()
+
+while True:
+    conn, addr = s.accept()
+    start_new_math_thread(conn, addr)
+s.close()
